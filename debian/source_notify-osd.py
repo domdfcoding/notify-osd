a6b8ded6e9c9739b25c8aa1cdf669001a3013ea8
Martin Pitt <martin.pitt@ubuntu.com>
2012-06-18 15:05:56 +0200

    0.9.34-0ubuntu3 (patches unapplied)
    Imported using git-ubuntu import.

--- debian/source_notify-osd.py
+++ debian/source_notify-osd.py
@@ -80,4 +80,4 @@ if __name__ == '__main__':
     report = {}
     add_info(report)
     for key in report:
-        print '[%s]\n%s' % (key, report[key])
+        print('[%s]\n%s' % (key, report[key]))
